deffa0efb7472fb64b6885cc5cdd0db497509008
fadhilrafiii <fadhilrafi90@gmail.com>
2022-09-23 15:39:20 +0700

    Add prettier and Setup DB connection

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import mongoose, { ConnectOptions } from 'mongoose';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("FADHIL APP API YEAY");
+app.get('/', (req: Request, res: Response) => {
+  res.send('FADHIL APP API YEAY');
 });
 
+// Connect to Database
+const databaseUrl: string = process.env.DATABASE_URL || '';
+mongoose.connect(databaseUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions);
+
+mongoose.connection.on('error', () => console.error('MongoDB Connection Error!'));
+
 app.listen(port, () => {
-  console.log("Server is running!");
+  console.log('Server is running!');
 });
