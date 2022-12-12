c2274d103b8abc0230543b876bb74891744681ba
fadhilrafiii <fadhil.muhammad@dansmultipro.com>
2022-12-12 18:05:29 +0700

    remove build dir

--- dist/constants/auth.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SESSION_EXPIRY_ONE_DAY = exports.JWT_EXPIRY_TWO_HOURS = void 0;
-exports.JWT_EXPIRY_TWO_HOURS = 1000 * 60 * 60 * 2;
-exports.SESSION_EXPIRY_ONE_DAY = 1000 * 60 * 60 * 24;
--- dist/helpers/validator/string.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isEmail = void 0;
-const isEmail = (email) => {
-    const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return emailPattern.test(email);
-};
-exports.isEmail = isEmail;
